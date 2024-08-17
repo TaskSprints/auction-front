796cd37db213967e8125f5de7d4e7de07aaae80a
KNU-K <knukang334@gmail.com>
2024-08-17 22:38:57 +0900

    init : 초기 구성 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-    return (
-        <div className="flex items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
 };
 export default App;
