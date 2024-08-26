14aa5bf3d57a7b2785eea94cd9183e898fe3d0ca
Jihun-Ha <wlgns359@gmail.com>
2024-08-26 10:33:03 +0900

    slick-slider가 모바일에서는 좌우 기본 슬라이드로 변경

--- src/components/body/AuctionInfo/index.tsx
+++ src/components/body/AuctionInfo/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import AuctionInfoList from "./HotAuction/AuctionInfoList";
-
 const AuctionInfo = () => {
   return (
-    <div className="flex flex-col justify-center mt-[2rem] p-2">
+    <div className="flex flex-col justify-center mt-[2rem] ">
       <AuctionInfoList />
+      <div className="h-[50rem]"></div>
     </div>
   );
 };
