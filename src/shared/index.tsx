b6ae2cc170af03a5c7a3e3b5d68450451678e394
KNU-K <knukang334@gmail.com>
2024-09-24 22:46:04 +0900

    fix : 일단 Product 와 관련된 부분 전체 주석 처리

--- src/shared/index.tsx
+++ src/shared/index.tsx
@@ -1,6 +1,5 @@
-export * from "./requests/products/products-api-client";
-export * from "./requests/api-instance-client";
+export * from "./api";
 
-export * from "./store/products/products-store";
+// export * from "./store/products/products-store";
 
 export * from "./types/products";
