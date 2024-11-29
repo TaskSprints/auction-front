564725d5b4fa8a694e29720fd7d6285f0573eb72
Kim Do Young <difbfl4750@gmail.com>
2024-11-29 16:19:54 +0900

    feat/ws 병합 및 gitignore 수정

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-const { resolve } = require("path");
+// import resolve from "path";
 
-async function example() {
-  console.log("첫 번째 출력"); // 동기적으로 실행됨
+// async function example() {
+//   console.log("첫 번째 출력"); // 동기적으로 실행됨
 
-  console.log("두 번째 출력"); // 대기가 끝난 후 실행됨
+//   console.log("두 번째 출력"); // 대기가 끝난 후 실행됨
 
-  await new Promise((resolve) => setTimeout(resolve, 1000)); // 다시 1초 대기
+//   await new Promise((resolve) => setTimeout(resolve, 1000)); // 다시 1초 대기
 
-  console.log("세 번째 출력"); // 두 번째 대기가 끝난 후 실행됨
-}
+//   console.log("세 번째 출력"); // 두 번째 대기가 끝난 후 실행됨
+// }
 
-example();
+// example();
