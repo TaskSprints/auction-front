bd02a9b750297fa19e39dd0d572a1e8fc8723c27
Kim Do Young <difbfl4750@gmail.com>
2024-11-30 17:41:11 +0900

    feat: favicon 설정 및 SEO를 위한 index.html 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SearchBar from "../widgets/Searchbar/SearchBar";
 import CardList from "../widgets/Card/CardList";
 import Header from "../widgets/Header/Header";
