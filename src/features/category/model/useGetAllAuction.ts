3107d6ecfac8a257e2cba584372582824280d4aa
Kim Do Young <difbfl4750@gmail.com>
2024-12-07 15:16:35 +0900

    feat: api 엔드포인트 변경

---
+++ src/features/category/model/useGetAllAuction.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { IAuction } from "@/shared/types/product.types";
+import { httpClient } from "@/shared/api/httpClient";
+
+const fetchAuctions = async (): Promise<IAuction[]> => {
+  const { data } = await httpClient.get("/api/v1/auction");
+  return data.data.content;
+};
+
+export const useGetAllAuction = () => {
+  const {
+    data: auctions,
+    isLoading: auctionIsLoading,
+    isError: auctionIsError,
+  } = useQuery({
+    queryKey: ["auctions"],
+    queryFn: fetchAuctions,
+  });
+
+  return { auctionIsLoading, auctionIsError, auctions };
+};
